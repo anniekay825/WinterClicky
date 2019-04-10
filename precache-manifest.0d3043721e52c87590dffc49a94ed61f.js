c38142287e0ae7c033f5a98699136fd39d65bc56
anniekay825@gmail.com <anniekay825@gmail.com>
2019-04-10 00:33:28 -0400

    Updates

---
+++ precache-manifest.0d3043721e52c87590dffc49a94ed61f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7961cf4aa35021942a6c",
+    "url": "/WinterClicky/static/css/main.8c03a418.chunk.css"
+  },
+  {
+    "revision": "7961cf4aa35021942a6c",
+    "url": "/WinterClicky/static/js/main.389d3d84.chunk.js"
+  },
+  {
+    "revision": "f820862e413b97abd307",
+    "url": "/WinterClicky/static/js/runtime~main.bb649837.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/WinterClicky/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "aa1d7f2d3489c1c2fc05cfaced983b8f",
+    "url": "/WinterClicky/index.html"
+  }
+];
